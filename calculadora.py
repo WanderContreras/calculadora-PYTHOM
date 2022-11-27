f769019f21e9669edf3f8da3ce9b00a68b232352
WanderContreras <wandercontrerasrosario@gmail.com>
2022-11-26 20:52:23 -0600

    se agrega el proyecto

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+
+print("Calculadora")
+n1 = input("primer numero: ")
+n2 = input("segundo numero: ")
+
+
+
+
+res = int(n1) + int(n2)
+res2 = int(n1) * int(n2)
+res3 = int(n1) - int(n2)
+
+print("------------------------------")
+print("suma", res)
+print("multiplicacion", res2)
+print("resta", res3)
+print("------------------------------")
